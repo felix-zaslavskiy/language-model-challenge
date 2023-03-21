10d176997d376e18130a175fa352c2c78b5bf432
felix <felix@zaslavskiy.net>
2023-03-21 00:40:24 -0400

    wip

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from typing import List
 
+NUMBER_OPTIONS=5
 class Note:
     def __init__(self, username: str, content: str, created_on: datetime):
         self.username = username
@@ -35,7 +36,7 @@ class Challenge:
         self.model_outputs.append(model_output)
 
 class MultipleChoiceChallenge(Challenge):
-    def __init__(self, title: str, question: str, options: List[str], correct_answer: str, explanation: str):
+    def __init__(self, title: str, question: str, options: dict[str], correct_answer: str, explanation: str):
         super().__init__(title)
         self.question = question
         self.options = options
@@ -83,16 +84,18 @@ class SubjectiveChallenge(Challenge):
 
 # Function to render multiple choice challenge
 def render_multiple_choice_challenge(challenge):
-    st.write(challenge["question"])
+    st.header(challenge["title"])
+    st.text(challenge["question"])
     st.write(challenge["options"])
-    st.write(challenge["correct_answer"])
-    st.write(challenge["explanation"])
+    st.text(challenge["correct_answer"])
+    st.text(challenge["explanation"])
 
 # Function to render subjective evaluation challenge
 def render_subjective_challenge(challenge):
-    st.write(challenge["prompt"])
-    st.write(challenge["summary"])
-    st.write(challenge["notes"])
+    st.header(challenge["title"])
+    st.text(challenge["prompt"])
+    st.text(challenge["summary"])
+    st.text(challenge["evaluation_criteria"])
 
 # Function to render challenge based on type
 def render_challenge(challenge):
@@ -103,43 +106,108 @@ def render_challenge(challenge):
 
 # Function to edit challenge
 def edit_challenge(challenge):
-    # Edit common properties
-    st.write("Edit challenge properties:")
+    with st.form("edit_challenge_form"):
+        st.write("Edit challenge properties:")
+        st.text_input("Title", value=challenge["title"], key="edit_title")
 
-    # Edit challenge-specific properties
-    if challenge["type"] == "multiple_choice":
-        st.write("Edit multiple choice challenge properties:")
-    elif challenge["type"] == "subjective":
-        st.write("Edit subjective challenge properties:")
+        if challenge["challenge_type"] == "multiple_choice":
+            st.write("Edit multiple choice challenge properties:")
+            st.text_area("Question", value=challenge["question"], key="edit_question")
+            create_option_inputs(NUMBER_OPTIONS)
 
-def add_new_challenge(new_challenge):
+            for i, option_key in enumerate(challenge["options"]):
+                st.session_state[f"option_{i+1}"] = challenge["options"][option_key]
+
+            st.selectbox("Correct Answer", options=create_options(NUMBER_OPTIONS), index=create_options(NUMBER_OPTIONS).index(challenge["correct_answer"]), key="edit_correct_answer")
+            st.text_area("Explanation", value=challenge["explanation"], key="edit_explanation")
+
+        elif challenge["challenge_type"] == "subjective":
+            st.write("Edit subjective challenge properties:")
+            st.text_area("Prompt", value=challenge["prompt"], key="edit_prompt")
+            st.text_area("Summary", value=challenge["summary"], key="edit_summary")
+            st.text_area("Evaluation Criteria", value=challenge["evaluation_criteria"], key="edit_evaluation_criteria")
+
+        if st.form_submit_button("Save Changes"):
+            challenge.title = st.session_state["edit_title"]
+
+            if challenge["challenge_type"] == "multiple_choice":
+                challenge.question = st.session_state["edit_question"]
+                challenge.options = get_options_dict(NUMBER_OPTIONS)
+                challenge.correct_answer = st.session_state["edit_correct_answer"]
+                challenge.explanation = st.session_state["edit_explanation"]
+
+            else:  # Subjective Challenge
+                challenge.prompt = st.session_state["edit_prompt"]
+                challenge.summary = st.session_state["edit_summary"]
+                challenge.evaluation_criteria = st.session_state["edit_evaluation_criteria"]
+
+            st.success("Challenge updated successfully!")
+            st.write("Switching to view mode...")
+            st.selectbox("Mode", options=["View", "Edit"], index=0)
+
+        if st.form_submit_button("Cancel"):
+            st.write("Switching to view mode without saving changes...")
+            st.selectbox("Mode", options=["View", "Edit"], index=0)
+
+def get_options_dict(num_options):
+    options_dict = {}
+    for i in range(num_options):
+        option_key = chr(ord("A") + i)
+        option_value = st.session_state[f"option_{i+1}"]
+        options_dict[option_key] = option_value
+    return options_dict
+
+def create_option_inputs(num_options):
+    option_inputs = []
+    for i in range(num_options):
+        option_key = chr(ord("A") + i)
+        option_input = st.text_input(f"Option {option_key}", "", key=f"option_{i+1}")
+        option_inputs.append(option_input)
+    return option_inputs
+
+def create_options(num_options):
+    options = []
+    for i in range(num_options):
+        option_label = chr(ord("A") + i)
+        options.append(option_label)
+    return options
+def add_new_challenge():
+
+    if st.session_state["challenge_type"] == "Multiple Choice":
+        options = get_options_dict(NUMBER_OPTIONS)
+        new_challenge = MultipleChoiceChallenge(st.session_state["title"],
+                                                st.session_state["question"], options,
+                                                st.session_state["correct_answer"],
+                                                st.session_state["explanation"])
+    else:  # Subjective Challenge
+        new_challenge = SubjectiveChallenge(st.session_state["title"],
+                                            st.session_state["prompt"],
+                                            st.session_state["summary"],
+                                            st.session_state["evaluation_criteria"])
+
     st.session_state["challenges"].append(new_challenge)
+
     st.sidebar.success("Challenge created successfully!")
     st.session_state["creating_new_challenge"] = False
 
 
 def create_new_challenge_modal():
-    with st.form("new_challenge_form"):
+    challenge_type = st.selectbox("Challenge Type", options=["Multiple Choice", "Subjective"], key="challenge_type")
+    with st.form("new_challenge_form", clear_on_submit=True):
         st.write("Create New Challenge")
-        challenge_type = st.selectbox("Challenge Type", options=["Multiple Choice", "Subjective"])
-        title = st.text_input("Title", "")
+        st.text_input("Title", "", key="title")
 
         if challenge_type == "Multiple Choice":
-            question = st.text_input("Question", "")
-            options = [st.text_input(f"Option {i+1}", "") for i in range(4)]
-            correct_answer = st.selectbox("Correct Answer", options=["A", "B", "C", "D"])
-            explanation = st.text_area("Explanation", "")
+            st.text_area("Question", "", key="question")
+            create_option_inputs(NUMBER_OPTIONS)
+            st.selectbox("Correct Answer", options=create_options(NUMBER_OPTIONS), key="correct_answer")
+            st.text_area("Explanation", "", key="explanation")
         else:  # Subjective Challenge
-            prompt = st.text_input("Prompt", "")
-            summary = st.text_area("Summary", "")
-            evaluation_criteria = st.text_area("Evaluation Criteria", "")
+            st.text_area("Prompt", "", key="prompt")
+            st.text_area("Summary", "", key="summary")
+            st.text_area("Evaluation Criteria", "", key="evaluation_criteria")
 
-        if challenge_type == "Multiple Choice":
-            new_challenge = MultipleChoiceChallenge(title, question, options, correct_answer, explanation)
-        else:  # Subjective Challenge
-            new_challenge = SubjectiveChallenge(title, prompt, summary, evaluation_criteria)
-
-        st.form_submit_button("Create Challenge", on_click=add_new_challenge, args=(new_challenge,))
+        st.form_submit_button("Create Challenge", on_click=add_new_challenge)
 
 
 # Main application
